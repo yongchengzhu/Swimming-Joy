9ba92f25c236ee22b14ecf229f16462707b3f9c0
yongchengzhu <yongchengzhu002@gmail.com>
2019-04-16 16:19:29 -0400

    Setup project for production.

---
+++ client/src/component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>App Component</div>
+        <a href="/backend">Link to Backend</a>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app     = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+// Routes Setup
+app.get('/backend', (req, res) => {
+  res.send('This is the backend server!');
 });
+
+// Production Setup
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
