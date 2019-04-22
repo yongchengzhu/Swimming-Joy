40a2fdb4cd3f52e612f5aef5a655f279bc0710c2
yongchengzhu <yongchengzhu002@gmail.com>
2019-04-22 14:37:44 -0400

    Added filter brightness to carousel images.

--- client/src/component/Test.js
+++ client/src/component/Test.js
@@ -7,26 +7,26 @@ class DemoCarousel extends Component {
     render() {
         return (
             <div>
-              <Carousel style={{ height: "100vh"/*, position: "absolute"*/, width: "100%", zIndex: "0" }}>
-                <Carousel.Item style={{ height: "100vh" }}>
+              <Carousel style={{ height: "100vh", width: "100%" }}>
+                <Carousel.Item style={{ height: "100vh", filter: "brightness(85%)" }}>
                   <img
                     className="d-block w-100"
                     src="https://www.thoughtco.com/thmb/ZE93UVJ2eVYbr_v5HUiyo7sRsl4=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/smooth-curve-and-water-jets-of-swimming-pool--641703906-5ade28fa43a1030036997d1a.jpg"
                   />
                 </Carousel.Item>
-                <Carousel.Item style={{ height: "100vh" }}>
+                <Carousel.Item style={{ height: "100vh", filter: "brightness(85%)" }}>
                   <img
                     className="d-block w-100"
                     src="https://water.arlingtonva.us/wp-content/uploads/sites/4/2016/03/Blue-Buoy-on-swimming-pool-000063897223_Large.jpg"
                   />
                 </Carousel.Item>
-                <Carousel.Item style={{ height: "100vh" }}>
+                <Carousel.Item style={{ height: "100vh", filter: "brightness(85%)" }}>
                   <img
                     className="d-block w-100"
                     src="https://www.stonybrook.edu/commcms/studentaffairs/rec/images/pool-2018.jpg"
                   />
                 </Carousel.Item>
-                <Carousel.Item style={{ height: "100vh" }}>
+                <Carousel.Item style={{ height: "100vh", filter: "brightness(85%)" }}>
                   <img
                     className="d-block w-100"
                     src="https://previews.123rf.com/images/epicstockmedia/epicstockmedia1507/epicstockmedia150700009/42845648-beautiful-luxury-home-with-swimming-pool-at-sunset.jpg"
