f33da887cc0b2beb9e85691d5cd2c95226e5b2b0
yongchengzhu <yongchengzhu002@gmail.com>
2019-04-27 13:04:27 -0400

    Used vw unit for font-size.

--- client/src/component/Programs.js
+++ client/src/component/Programs.js
@@ -59,15 +59,15 @@ class Programs extends React.Component {
                         <div className="col">
                           <i className="fas fa-life-ring" style={{ margin: "20% 0", fontSize: "9em" }} />
                           <div style={{ textAlign: "left" }}>
-                            <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Lifeguard Training</Header>
-                            <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66", textAlign: "center" }}>
+                            <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Lifeguard Training</Header>
+                            <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66", textAlign: "center", fontSize: "1.1vw" }}>
                               <p style={{ textAlign: "left" }}>Effective trainings and guidances from our professional trainers.</p>
                               <ul style={{ textAlign: "left" }}>
                                 <li>40 Hours of Training</li>
                                 <li>CPR Course</li>
                                 <li>Swimming & Written Exams</li>
                               </ul>
-                              <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$250.00</p>
+                              <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px", fontSize: "1.1vw" }}>$250.00</p>
                               <Button onClick={ () => this.onButtonClick('Lifeguard Training', 1, 250) } style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                             </div>
                           </div>
@@ -79,15 +79,15 @@ class Programs extends React.Component {
                       <div className="col">
                         <i className="fas fa-swimmer" style={{ margin: "20% 0", fontSize: "9em" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Adults Program</Header>
-                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66", textAlign: "center" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Adults Program</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66", textAlign: "center", fontSize: "1.1vw" }}>
                             <p style={{ textAlign: "left" }}>Trainee must be over the age of 18. Learn to swim in less than a month.</p>
                             <ul style={{ textAlign: "left" }}>
                               <li>8 Lessons</li>
                               <li>Basic Level Swimming</li>
                               <li>Safety Skills</li>
                             </ul>
-                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$175.00</p>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px", fontSize: "1.1vw" }}>$175.00</p>
                             <Button onClick={ () => this.onButtonClick('Adults Program', 1, 175) } primary style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                           </div>
                         </div>
@@ -99,15 +99,15 @@ class Programs extends React.Component {
                       <div className="col">
                         <i className="fas fa-running" style={{ margin: "20% 0", fontSize: "9em" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Teens Program</Header>
-                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66", textAlign: "center" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Teens Program</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66", textAlign: "center", fontSize: "1.1vw" }}>
                             <p style={{ textAlign: "left" }}>Trainee must be 10-17 years old. Learn basic swimming techniques.</p>
                             <ul style={{ textAlign: "left" }}>
                               <li>8 Lessons</li>
                               <li>Swim Without Assistance</li>
                               <li>Athletic Trainings</li>
                             </ul>
-                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$170.00</p>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px", fontSize: "1.1vw" }}>$170.00</p>
                             <Button onClick={ () => this.onButtonClick('Teens Program', 1, 170) } primary style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                           </div>
                         </div>
@@ -122,15 +122,15 @@ class Programs extends React.Component {
                       <div className="col">
                         <i className="fas fa-child" style={{ margin: "20% 0", fontSize: "9em" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Kids Program</Header>
-                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Kids Program</Header>
+                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66", fontSize: "1.1vw" }}>
                             <p style={{ textAlign: "left" }}>Trainee must be 6-9 years old. Learn basic aquatic skills.</p>
                             <ul style={{ textAlign: "left" }}>
                               <li>8 Lessons</li>
                               <li>Swim Assistance</li>
                               <li>Breath Control</li>
                             </ul>
-                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$165.00</p>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px", fontSize: "1.1vw" }}>$165.00</p>
                             <Button onClick={ () => this.onButtonClick('Kids Program', 1, 165) } primary style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                           </div>
                         </div>
@@ -142,15 +142,15 @@ class Programs extends React.Component {
                       <div className="col">
                         <i className="fas fa-baby" style={{ margin: "20% 0", fontSize: "9em" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Mom & Baby Class</Header>
-                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Mom & Baby Class</Header>
+                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66", fontSize: "1.1vw" }}>
                             <p style={{ textAlign: "left" }}>Introductory swimming course for toddlers and parents.</p>
                             <ul style={{ textAlign: "left" }}>
                               <li>8 Lessons</li>
                               <li>Parents / Guardians required</li>
                               <li>Get toddler familiar with water</li>
                             </ul>
-                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$190.00</p>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px", fontSize: "1.1vw" }}>$190.00</p>
                             <Button onClick={ () => this.onButtonClick('Mom & Baby Class', 1, 190) } primary style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                           </div>
                         </div>
@@ -162,15 +162,15 @@ class Programs extends React.Component {
                       <div className="col">
                         <i className="fas fa-glass-cheers" style={{ margin: "20% 0", fontSize: "9em" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Boys' & Girls' Night</Header>
-                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Boys' & Girls' Night</Header>
+                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66", fontSize: "1.1vw" }}>
                             <p style={{ textAlign: "left" }}>Reserve our spacious campus for the beautiful night.</p>
                             <ul style={{ textAlign: "left" }}>
                               <li>Maximum 50 people</li>
                               <li>No age restrictions</li>
                               <li>Time to Party!</li>
                             </ul>
-                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$100.00</p>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px", fontSize: "1.1vw" }}>$100.00</p>
                             <Button onClick={ () => this.onButtonClick("Boys' & Girls' Night", 1, 100) } primary style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                           </div>
                         </div>
@@ -185,14 +185,14 @@ class Programs extends React.Component {
                       <div className="col">
                         <i className="fas fa-swimming-pool" style={{ margin: "20% 0", fontSize: "9em" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Adults Membership</Header>
-                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700", fontSize: "1.8vw" }}>Adults Membership</Header>
+                          <div style={{ textAlign: "center", fontFamily: "MuseoW01-700", color: "#1A4C66", fontSize: "1.1vw" }}>
                             <p style={{ textAlign: "left" }}>Keep up with your swimming everyday.</p>
                             <ul style={{ textAlign: "left" }}>
                               <li>Unlimited Facility Access</li>
                               <li>Personal Trainer</li>
                             </ul>
-                            <p style={{ textAlign: "center", fontSize: "15px" }}>$25.00</p>
+                            <p style={{ textAlign: "center", fontSize: "15px", fontSize: "1.1vw" }}>$25.00</p>
                             <Button onClick={ () => this.onButtonClick('Adults Membership', 1, 25) } primary style={{ marginBottom: "10px", borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                           </div>
                         </div>
