c303abd0c504cae92d92182ecd75d2c0012b021a
yongchengzhu <yongchengzhu002@gmail.com>
2019-04-27 11:59:01 -0400

    Cleaned up code in program page, and styled fonts. Styled About page.

--- client/src/component/About.js
+++ client/src/component/About.js
@@ -26,11 +26,13 @@ class About extends React.Component {
         
 
         {/* Content */}
-          <div className="container-fluid" style={{ padding: "0 15em", background: "url('https://static.wixstatic.com/media/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.png/v1/fill/w_408,h_408,al_c,q_85/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.webp')", backgroundColor: "rgb(43, 158, 217)" }}>
+        <div style={{ background: "url('https://static.wixstatic.com/media/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.png/v1/fill/w_408,h_408,al_c,q_85/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.webp')", backgroundColor: "rgb(43, 158, 217)" }}>
+          {/* About Us */}
+          <div className="container-fluid" style={{ padding: "0 15em"}}>
             <div className="row">
               <div className="col" style={{ textAlign: "center", margin: "4em 0em" }}>
-                <p style={{ fontSize: "3em", fontWeight: "100" }}>About Us</p>
-                <p style={{ fontWeight: "100", fontSize: "18px" }}>Joy Swimming Inc is a standard and well-equipped chlorinated 25 yards 
+                <p style={{ fontSize: "3em", fontWeight: "100", fontFamily: "MuseoW01-700", color: "white" }}>About Us</p>
+                <p style={{ fontWeight: "100", fontSize: "18px", color: "white" }}>Joy Swimming Inc is a standard and well-equipped chlorinated 25 yards 
                   indoor swimming pool located in the heart of the rockaway’s towns. Our main 
                   goal on setting this business is with the aim to engage and encourage parents 
                   and as well kids to participate in sport activity since one’s promotes health, 
@@ -43,9 +45,50 @@ class About extends React.Component {
               </div>
             </div>
           </div>
-          
+
+          {/* Separator */}
+          <div style={{ width: "100%", height: "37px", backgroundSize: "auto", backgroundPosition: "center center", backgroundImage: "url('https://static.wixstatic.com/media/84770f_effc02db3b64431fa384e9028bc63812~mv2.png/v1/crop/x_0,y_0,w_218,h_33,q_85/84770f_effc02db3b64431fa384e9028bc63812~mv2.webp')" }}></div>
+        
+          {/* Our Mission */}
+          <div className="container-fluid" style={{ padding: "0 15em"}}>
+            <div className="row">
+              <div className="col" style={{ textAlign: "center", margin: "4em 0em" }}>
+                <p style={{ fontSize: "3em", fontWeight: "100", fontFamily: "MuseoW01-700", color: "white" }}>Our Mission</p>
+                <p style={{ fontWeight: "100", fontSize: "18px", color: "white" }}>
+                  JSI's mission is to provide a sport facility that enhances the health, fitness, 
+                  safety, recreation and quality of life for all in Joy Swimming Inc. An indoor 
+                  pool facility which will include multiple swimming activities such as competition, 
+                  water challenges and other related activities and engage commuters in sporting 
+                  activities, health and fitness and to fight against obesity and unhealthy lifestyle.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Separator */}
+          <div style={{ width: "100%", height: "37px", backgroundSize: "auto", backgroundPosition: "center center", backgroundImage: "url('https://static.wixstatic.com/media/84770f_effc02db3b64431fa384e9028bc63812~mv2.png/v1/crop/x_0,y_0,w_218,h_33,q_85/84770f_effc02db3b64431fa384e9028bc63812~mv2.webp')" }}></div>
+        
+          {/* Open Hours */}
+          <div className="container-fluid" style={{ padding: "0 15em"}}>
+            <div className="row">
+              <div className="col" style={{ textAlign: "center", margin: "4em 0em" }}>
+                <p style={{ fontSize: "3em", fontWeight: "100", fontFamily: "MuseoW01-700", color: "white" }}>Open Hours</p>
+                <p style={{ fontWeight: "100", fontSize: "18px", color: "white" }}>
+                  Joy Swimming Inc will be opened Monday - Thursday from 8AM - 6PM weekends 
+                  (Fridays to Sundays) and from 7:00 am to 6:00 pm to offer the most flexibles 
+                  schedule and for us to be able to accommodate customer with different time 
+                  preference also to recruit enough staff to run shift among our workforce. We will 
+                  go out of our way to acquire standard equipment and other customers’ needs or 
+                  requests to keep them happy and be able to provide a healthier environment. 
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
           {/* Absolute Position Content */}
-          <div className="container-fluid" style={{ backgroundColor: "seashell"/*, position: "absolute"*/, left: "0", opacity: "0.8" }}>
+          
+          {/* <div className="container-fluid" style={{ backgroundColor: "seashell", left: "0", opacity: "0.8" }}>
             <div className="row align-items-center" style={{ height: "40vh" }}>
               <div className="col" style={{ padding: "0 15em" }}>
                 <p style={{ fontSize: "20px", fontWeight: "600" }}>OUR MISSION:</p>
@@ -59,10 +102,11 @@ class About extends React.Component {
                 </p>
               </div>
             </div>
-          </div>
+          </div> */}
+          
 
-          {/* Bottom Content */}
-          <div className="container-fluid" style={{ padding: "0 15em", background: "url('https://static.wixstatic.com/media/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.png/v1/fill/w_408,h_408,al_c,q_85/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.webp')", backgroundColor: "rgb(43, 158, 217)" }}>
+          
+          {/* <div className="container-fluid" style={{ padding: "0 15em", background: "url('https://static.wixstatic.com/media/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.png/v1/fill/w_408,h_408,al_c,q_85/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.webp')", backgroundColor: "rgb(43, 158, 217)" }}>
             <div className="row">
               <div className="col" style={{ textAlign: "center", margin: "4em 0em" }}>
                 <p style={{ fontSize: "3em", fontWeight: "100" }}>Open Hours</p>
@@ -76,7 +120,7 @@ class About extends React.Component {
                 </p>
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* <Container text style={{ marginTop: "4em", marginBottom: '4em' }}>
             <Header as="h1">About</Header>
--- client/src/component/Programs.js
+++ client/src/component/Programs.js
@@ -46,172 +46,173 @@ class Programs extends React.Component {
           <div className="container-fluid" style={{ paddingBottom: "2em", fontWeight: "100", background: "url('https://static.wixstatic.com/media/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.png/v1/fill/w_408,h_408,al_c,q_85/a9ff3b_38e5f9b5a82546a689f8013ed6fce03b.webp')", backgroundColor: "rgb(43, 158, 217)" }}>
             <div className="row" >
               <div className="col" style={{ textAlign: "center", margin: "4em 0em" }}>
-                <p style={{ fontSize: "3em", fontWeight: "100" }}>Something for Everyone</p>
-                <p style={{ fontWeight: "100" }}>Whether you want to train to become an athlete, a lifeguard, or just to workout, we have you covered. </p>
+                <p style={{ fontSize: "57px", fontFamily: "MuseoW01-700", color: "white" }}>Something for Everyone</p>
+                <p style={{ fontSize: "18px", fontFamily: "MuseoW01-700", color: "white" }}>Whether you want to train to become an athlete, a lifeguard, or just to workout, we have you covered. </p>
               </div>
             </div>
             <div className="row align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-              <div className="col-auto" style={{ textAlign: "center", backgroundColor: "white", borderRadius: "25px" }}>
+              <div className="col-10" style={{ textAlign: "center", backgroundColor: "white", borderRadius: "25px" }}>
                 {/* First row */}
                 <div className="row justify-content-center" style={{ margin: "1em 0em", height: "35em" }}>
-                  <div className="col" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px" }}>
-                      <div className="row align-items-start" style={{ /*marginTop: "4em",*/ height: "100%", padding: "0 1em" }}>
-                        
+                  <div className="col" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px" }}>
+                      <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
                         <div className="col">
                           <i className="fas fa-life-ring" style={{ marginTop: "20%", marginBottom: "23%", fontSize: "9em" /*, position: "absolute", top: "50px", left: "0", right: "0"*/ }} />
                           <div style={{ textAlign: "left" }}>
-                            <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Lifeguard Training</Header>
-                            <div style={{ /*position: "absolute"*/ }}>
+                            <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Lifeguard Training</Header>
+                            <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
                               <p>Effective trainings and guidances from our professional trainers.</p>
                               <ul>
                                 <li>40 Hours of Training</li>
                                 <li>CPR Course</li>
                                 <li>Swimming & Written Exams</li>
                               </ul>
+                              <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$250.00</p>
                             </div>
                           </div>
                         </div>
                         <div style={{ position: "absolute", bottom:"20px", left: "0", right: "0", textAlign: "center" }}>
-                              <Header as="h3" style={{ fontWeight: "100" }}>$250.00</Header>
-                              <Button onClick={ () => this.onButtonClick('Lifeguard Training', 1, 250) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                              
+                              <Button onClick={ () => this.onButtonClick('Lifeguard Training', 1, 250) } style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                         </div>
                       </div>
                   </div>
-                  <div className="col" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px" }}>
-                    <div className="row align-items-center" style={{ height: "100%", padding: "0 1em" }}>
-                      <i className="fas fa-swimmer" style={{ fontSize: "9em", position: "absolute", top: "50px", left: "0", right: "0" }} />
+                  <div className="col" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px" }}>
+                    <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
                       <div className="col">
+                        <i className="fas fa-swimmer" style={{ fontSize: "9em", marginBottom: "23%", marginTop: "20%" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Adults Program</Header>
-                          <div style={{ position: "absolute" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Adults Program</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
                             <p>Trainee must be over the age of 18. Learn to swim in less than a month.</p>
                             <ul>
                               <li>8 Lessons</li>
                               <li>Basic Level Swimming</li>
                               <li>Safety Skills</li>
                             </ul>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$175.00</p>
                           </div>
                         </div>
                       </div>
                       <div style={{ position: "absolute", bottom:"20px", left: "0", right: "0" }}>
-                            <Header as="h3" style={{ fontWeight: "100" }}>$175.00</Header>
-                            <Button onClick={ () => this.onButtonClick('Adults Program', 1, 175) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                            
+                            <Button onClick={ () => this.onButtonClick('Adults Program', 1, 175) } primary style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                       </div>
                     </div>
                   </div>
-                  <div className="col" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px" }}>
-                    <div className="row align-items-center" style={{ height: "100%", padding: "0 1em" }}>
-                      <i className="fas fa-running" style={{ fontSize: "9em", position: "absolute", top: "50px", left: "0", right: "0" }} />
+                  <div className="col" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px" }}>
+                    <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
                       <div className="col">
+                        <i className="fas fa-running" style={{ fontSize: "9em", marginBottom: "23%", marginTop: "20%" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Teens Program</Header>
-                          <div style={{ position: "absolute" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Teens Program</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
                             <p>Trainee must be 10-17 years old. Learn basic swimming techniques.</p>
                             <ul>
                               <li>8 Lessons</li>
                               <li>Swim Without Assistance</li>
                               <li>Athletic Trainings</li>
                             </ul>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$170.00</p>
                           </div>
                         </div>
                       </div>
                       <div style={{ position: "absolute", bottom:"20px", left: "0", right: "0" }}>
-                            <Header as="h3" style={{ fontWeight: "100" }}>$170.00</Header>
-                            <Button onClick={ () => this.onButtonClick('Teens Program', 1, 170) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                            <Button onClick={ () => this.onButtonClick('Teens Program', 1, 170) } primary style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                       </div>
                     </div>
                   </div>
                 </div>
                 {/* Second Row */}
                 <div className="row justify-content-center" style={{ margin: "1em 0em", height: "35em" }}>
-                  <div className="col" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px" }}>
-                    <div className="row align-items-center" style={{ height: "100%", padding: "0 1em" }}>
-                      <i className="fas fa-child" style={{ fontSize: "9em", position: "absolute", top: "50px", left: "0", right: "0" }} />
+                  <div className="col" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px" }}>
+                    <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
                       <div className="col">
+                        <i className="fas fa-child" style={{ fontSize: "9em", marginBottom: "23%", marginTop: "20%" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Kids Program</Header>
-                          <div style={{ position: "absolute" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Kids Program</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
                             <p>Trainee must be 6-9 years old. Learn basic aquatic skills.</p>
                             <ul>
                               <li>8 Lessons</li>
                               <li>Swim Assistance</li>
                               <li>Breath Control</li>
                             </ul>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$165.00</p>
                           </div>
                         </div>
                       </div>
                       <div style={{ position: "absolute", bottom:"20px", left: "0", right: "0" }}>
-                            <Header as="h3" style={{ fontWeight: "100" }}>$165.00</Header>
-                            <Button onClick={ () => this.onButtonClick('Kids Program', 1, 165) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                            <Button onClick={ () => this.onButtonClick('Kids Program', 1, 165) } primary style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                       </div>
                     </div>
                   </div>
-                  <div className="col" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px" }}>
-                    <div className="row align-items-center" style={{ height: "100%", padding: "0 1em" }}>
-                      <i className="fas fa-baby" style={{ fontSize: "9em", position: "absolute", top: "50px", left: "0", right: "0" }} />
+                  <div className="col" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px" }}>
+                    <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
                       <div className="col">
+                        <i className="fas fa-baby" style={{ fontSize: "9em", marginBottom: "23%", marginTop: "20%" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Mom & Baby Class</Header>
-                          <div style={{ position: "absolute" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Mom & Baby Class</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
                             <p>Introductory swimming course for toddlers and parents.</p>
                             <ul>
                               <li>8 Lessons</li>
                               <li>Parents / Guardians required</li>
                               <li>Get toddler familiar with water</li>
                             </ul>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$190.00</p>
                           </div>
                         </div>
                       </div>
                       <div style={{ position: "absolute", bottom:"20px", left: "0", right: "0" }}>
-                            <Header as="h3" style={{ fontWeight: "100" }}>$190.00</Header>
-                            <Button onClick={ () => this.onButtonClick('Mom & Baby Class', 1, 190) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                            <Button onClick={ () => this.onButtonClick('Mom & Baby Class', 1, 190) } primary style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                       </div>
                     </div>
                   </div>
-                  <div className="col" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px" }}>
-                    <div className="row align-items-center" style={{ height: "100%", padding: "0 1em" }}>
-                      <i className="fas fa-glass-cheers" style={{ fontSize: "9em", position: "absolute", top: "50px", left: "0", right: "0" }} />
+                  <div className="col" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px" }}>
+                    <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
                       <div className="col">
+                        <i className="fas fa-glass-cheers" style={{ fontSize: "9em", marginBottom: "23%", marginTop: "20%" }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Boys' & Girls' Night</Header>
-                          <div style={{ position: "absolute" }}>
-                            <p>Reserve our spacious campus for the night.</p>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Boys' & Girls' Night</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
+                            <p>Reserve our spacious campus for the beautiful night.</p>
                             <ul>
                               <li>Maximum 50 people</li>
                               <li>No age restrictions</li>
                               <li>Time to Party!</li>
                             </ul>
+                            <p style={{ textAlign: "center", marginTop: "0", fontSize: "15px" }}>$100.00</p>
                           </div>
                         </div>
                       </div>
                       <div style={{ position: "absolute", bottom:"20px", left: "0", right: "0" }}>
-                            <Header as="h3" style={{ fontWeight: "100" }}>$100.00</Header>
-                            <Button onClick={ () => this.onButtonClick("Boys' & Girls' Night", 1, 100) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                            <Button onClick={ () => this.onButtonClick("Boys' & Girls' Night", 1, 100) } primary style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                       </div>
                     </div>
                   </div>
                 </div>
                 {/* Third Row */}
                 <div className="row justify-content-center" style={{ margin: "1em 0em", height: "35em" }}>
-                  <div className="" style={{ border: "1px solid lightgrey", margin: "0em 1em", borderRadius: "25px", width: "31%" }}>
+                  <div className="" style={{ border: "1px solid #2B9ED9", margin: "0em 1em", borderRadius: "25px", width: "31%" }}>
                     <div className="row align-items-start" style={{ height: "100%", padding: "0 1em" }}>
-                      
                       <div className="col">
                         <i className="fas fa-swimming-pool" style={{ marginTop: "20%", marginBottom: "23%", fontSize: "9em"/*, position: "absolute", top: "50px", left: "0", right: "0"*/ }} />
                         <div style={{ textAlign: "left" }}>
-                          <Header as="h2" style={{ textAlign: "center", fontWeight: "100" }}>Adults Membership</Header>
-                          <div style={{ position: "absolute" }}>
+                          <Header as="h2" style={{ textAlign: "center", color: "#2B9ED9", fontFamily: "MuseoW01-700" }}>Adults Membership</Header>
+                          <div style={{ fontFamily: "MuseoW01-700", color: "#1A4C66" }}>
                             <p>Keep up with your swimming everyday.</p>
                             <ul>
                               <li>Unlimited Facility Access</li>
                               <li>Personal Trainer</li>
                             </ul>
+                            <p style={{ textAlign: "center", fontSize: "15px" }}>$25.00</p>
                           </div>
                         </div>
                       </div>
                       <div style={{ position: "absolute", bottom:"35px", left: "0", right: "0" }}>
-                            <Header as="h3" style={{ fontWeight: "100" }}>$25.00</Header>
-                            <Button onClick={ () => this.onButtonClick('Adults Membership', 1, 25) } primary style={{ borderRadius:"25px", width: "10em" }}>SELECT</Button>
+                            
+                            <Button onClick={ () => this.onButtonClick('Adults Membership', 1, 25) } primary style={{ borderRadius:"25px", width: "10em", background: "#5CC5FD", color: "#FBFBFB", fontFamily: "MuseoW01-700" }}>SELECT</Button>
                       </div>
                     </div>
                   </div>
